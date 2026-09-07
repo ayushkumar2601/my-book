7454fe38d5cd5e69dbbffa3b269078a3882bb2ce
ayushkumar2601 <kumarayush.professional@gmail.com>
2026-09-07 15:29:41 +0530

    Update folder icon card previews to gallery images 21, 11, and 9

--- src/components/bento/FolderIcon.tsx
+++ src/components/bento/FolderIcon.tsx
@@ -33,7 +33,7 @@ export const FolderIcon = () => {
                     {/* Doc 3 (Left / Back) */}
                     <div className="absolute bottom-0 left-0 w-16 h-20 bg-neutral-800 dark:bg-neutral-800 rounded-md shadow-md border border-white/20 transform origin-bottom transition-all duration-300 ease-out will-change-transform group-hover:-translate-x-6 group-hover:-translate-y-4 group-hover:-rotate-16 flex flex-col p-1 justify-between overflow-hidden">
                         <div className="w-full h-12 overflow-hidden rounded">
-                            <img src="/gallery/1.jpeg" alt="Gallery preview 1" className="w-full h-full object-cover" />
+                            <img src="/gallery/21.jpeg" alt="Gallery preview 21" className="w-full h-full object-cover" />
                         </div>
                         <div className="space-y-0.5 px-0.5 pb-0.5">
                             <div className="w-3/4 h-1 bg-white/40 rounded"></div>
@@ -44,7 +44,7 @@ export const FolderIcon = () => {
                     {/* Doc 2 (Center / Middle) */}
                     <div className="absolute bottom-0 left-2 w-16 h-20 bg-neutral-800 dark:bg-neutral-800 rounded-md shadow-md border border-white/20 transform origin-bottom transition-all duration-300 ease-out will-change-transform group-hover:-translate-y-7 flex flex-col p-1 justify-between overflow-hidden">
                         <div className="w-full h-12 overflow-hidden rounded">
-                            <img src="/gallery/2.jpeg" alt="Gallery preview 2" className="w-full h-full object-cover" />
+                            <img src="/gallery/11.jpeg" alt="Gallery preview 11" className="w-full h-full object-cover" />
                         </div>
                         <div className="space-y-0.5 px-0.5 pb-0.5">
                             <div className="w-4/5 h-1 bg-white/40 rounded"></div>
@@ -55,7 +55,7 @@ export const FolderIcon = () => {
                     {/* Doc 1 (Right / Front) */}
                     <div className="absolute bottom-0 left-4 w-16 h-20 bg-neutral-800 dark:bg-neutral-800 rounded-md shadow-lg border border-white/20 transform origin-bottom transition-all duration-300 ease-out will-change-transform group-hover:translate-x-6 group-hover:-translate-y-4 group-hover:rotate-16 flex flex-col p-1 justify-between overflow-hidden">
                         <div className="w-full h-12 overflow-hidden rounded">
-                            <img src="/gallery/3.jpeg" alt="Gallery preview 3" className="w-full h-full object-cover" />
+                            <img src="/gallery/9.jpeg" alt="Gallery preview 9" className="w-full h-full object-cover" />
                         </div>
                         <div className="space-y-0.5 px-0.5 pb-0.5">
                             <div className="w-2/3 h-1 bg-white/40 rounded"></div>
